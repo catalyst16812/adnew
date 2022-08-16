428843d77f3e74cf71d7a90e6c6dd710987f7b35
Nikhil <Nikhil@DESKTOP-0Q9LF51>
2022-08-16 16:18:18 +0530

    adnew

--- questions/src/questions/chessfind.java
+++ questions/src/questions/chessfind.java
@@ -4,7 +4,10 @@ import java.util.Scanner;
 
 public class chessfind {
 	static Scanner sc= new Scanner(System.in);
+	
+	
 	public static void main(String[] args) {
+		
 		System.out.println("input your square");
 		String a = sc.nextLine();
 		int pos = (int)a.charAt(1) + (int)a.charAt(0);
@@ -12,6 +15,7 @@ public class chessfind {
 			System.out.println("Black");
 		else
 			System.out.println("White");	
+		
 	}
 
 }
